2864ef6acbcf54f6cfe8659a89da96edd9e9864c
Akhil A <akhilarun60@gmail.com>
2025-07-20 23:22:49 +0530

    Connected MongoDB & added Task model and routes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 5000;
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+const taskRoutes = require('./routes/taskRoutes');
+app.use('/api/tasks', taskRoutes);
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Task Tracker API is running!");
 });
